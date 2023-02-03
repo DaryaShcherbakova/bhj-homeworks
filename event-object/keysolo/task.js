3f8d87fa2ef5b001412881f15e4faf9dcf7815e8
Darya Shcherbakova <darya0022@yandex.ru>
2023-02-03 18:21:08 +0300

    второе задание выполнено

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+   document.addEventListener("keyup",(event) => {
+    if(event.key.toLocaleLowerCase() === this.currentSymbol.textContent.toLocaleLowerCase()) {
+      this.success();
+    }
+    else {
+      this.fail();
+    }
+   });
   }
 
   success() {
